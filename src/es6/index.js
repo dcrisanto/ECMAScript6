5f880a460f9c84a05136f196fe05dfb90e7a080c
dcrisanto <dorelly.crisanto@gmail.com>
2022-08-02 04:33:59 -0500

    ECMAScript6: default params, concatenación, let, const, multilínea, spread operator, destructuración, arrow functions, promesas y parámetros en objetos

---
+++ src/es6/index.js
@@ -0,0 +1,160 @@
+function newFunction(name, age, country){
+    var name = name || 'Oscar';
+    var age = age || '31';
+    var country = country || 'Sullana';
+    console.log(name, age, country);
+}
+
+// es6
+function newFunction2(name='Oscar', age='31', country='Sullana'){
+    console.log(name, age, country);
+}
+
+newFunction2();
+newFunction2('Roberto', '23', 'Colombia');
+
+// template literal
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+// es6
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2);
+
+// multilínea
+let lorem = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet \n" + "consectetur, adipisci velit... There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...";
+console.log(lorem);
+
+//es6
+let lorem2 =`Neque porro quisquam est qui dolorem ipsum quia dolor sit amet
+consectetur, adipisci velit... There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...
+`;
+console.log(lorem2);
+
+// Destructuración
+let person = {
+    name: 'Dorelly',
+    age: '31',
+    country: 'Sullana'
+}
+
+console.log(person.name, person.age, person.country);
+
+//es6
+let { name, age, country } = person;
+console.log(name, age, country);
+
+// Operador de propagación: es6
+let team1 = ['Oscar', 'Juliam', 'Fabian'];
+let team2 = ['Valeria', 'Jessica', 'Camila'];
+let education = ['David', ...team1, ...team2];
+console.log(education);
+
+// variables: disponible en forma global
+var hola = 'hola';
+let holaLet = 'holaLet';
+console.log(holaLet);
+// let: sólo disponible en el scope, en el bloque de código que será llamado: es6
+{
+    var globalVar = 'Global Var';
+}
+
+{
+    let globalLet = 'Global Let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+ 
+//var
+var a = 'b';
+a = 'a';
+console.log(a);
+
+//es6 const: asignar variable sin reasignar
+const ax = 'b';
+//a = 'a'; // no found reasignar
+
+// objectos
+const completName = 'Dorelly Crisanto';
+const ageUser = '33';
+
+//es5
+var obj = {
+    completName,
+    ageUser
+}
+
+console.log(obj);
+//es6
+const obj2 = { name, age };
+
+console.log(obj2);
+
+//es6: arrow functions
+const names = [
+    {
+        name: 'Dorelly',
+        age: '31',
+        country: 'Sullana'
+    },
+    {
+        name: 'Roxana',
+        age: '32',
+        country: 'Piura'
+    }
+]
+//es5
+let listOfNames = names.map(function(item){ //objeto+método+función_anónima(elemento)
+    console.log(item.name);
+});
+//es6
+let listOfNames2 =names.map(item=>{ //establecemos el elemento que necesitamos y que retornará algo
+    console.log(item.name);
+})
+
+const listOfNames3 = (name, age, country) => {
+   console.log('arrow function recibiendo parámetros');
+};
+
+const listOfNames4 = name => {
+    console.log('arrow function recibiendo un sólo parámetro');
+};
+
+const square = num => num*num;
+
+console.log(square(4));
+
+// Promise: algo va a pasar
+const helloPromise = () => {
+    return new Promise((resolve, reject) => { // la promesa va a resolver o va hacer rechazada
+        if(true){
+            resolve('Hey!')
+        } else {
+            reject('Ups!!');
+        }
+    });
+}
+
+helloPromise()
+    .then(response => console.log(response)) //obtener la respuesta
+    .then(()=> console.log('hola')) //podemos anidar varios elementos then
+    .catch(error => console.log(error)) //obtener el error
+
+    const cows = 15;
+
+    const countCows =  () => {
+        return new Promise((resolve, reject) => {
+            if(cows>10){
+                resolve(`We have ${cows} cows on the farm`)
+            } else {
+                reject("There is no cows on the farm")
+            }
+        })
+    }
+
+    countCows()
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
